9b20e987d7de6e0a2dc7a82e3d63763085860182
enrique.cervirodam@gmail.com <enrique.cervirodam@gmail.com>
2022-05-02 13:01:19 +0200

    Repository Interface creado para NationalPark, métodos basicos de listado, borrado, update, create.

---
+++ ParkyAPI/Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ParkyAPI.Models;
+
+namespace ParkyAPI.Repository.IRepository
+{
+    public interface INationalParkRepository
+    {
+        ICollection<NationalPark> getNationalParks();
+        NationalPark getNationalPark(int nationalParkId);
+
+        bool NationalParkExists(string name);
+
+        bool NationalParkExists(int id);
+
+        bool Save();
+
+        bool CreateNationalPark(NationalPark nationalPark);
+
+        bool UpdateNationalPark(NationalPark nationalPark);
+
+        bool DeleteNationalPark(NationalPark nationalPark);
+
+
+    }
+}
---
+++ ParkyAPI/Repository/NationalParkRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using ParkyAPI.Data;
+using ParkyAPI.Models;
+using ParkyAPI.Repository.IRepository;
+
+namespace ParkyAPI.Repository
+{
+    public class NationalParkRepository : INationalParkRepository
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public NationalParkRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ICollection<NationalPark> getNationalParks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public NationalPark getNationalPark(int nationalParkId)
+        {
+            return _db.NationalParks.FirstOrDefault(x => x.Id == nationalParkId);
+        }
+
+        public bool NationalParkExists(string name)
+        {
+            return _db.NationalParks.Any(x => x.Name.ToLower().Trim() == name.ToLower().Trim());
+        }
+
+        public bool NationalParkExists(int id)
+        {
+            return _db.NationalParks.Any(x => x.Id == id);
+        }
+
+        public bool Save()
+        {
+            return _db.SaveChanges() >= 0 ? true : false;
+        }
+
+        #region CRUD
+        public bool CreateNationalPark(NationalPark nationalPark)
+        {
+            _db.NationalParks.Add(nationalPark);
+            return Save();
+        }
+
+        public bool UpdateNationalPark(NationalPark nationalPark)
+        {
+            _db.NationalParks.Update(nationalPark);
+            return Save();
+        }
+
+        public bool DeleteNationalPark(NationalPark nationalPark)
+        {
+            _db.NationalParks.Remove(nationalPark);
+            return Save();
+        }
+
+        #endregion
+
+    }
+}
